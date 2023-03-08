50b65f75f5b20edf6e344b7bf27976ec00e5b58a
sam40305sam <sam40305sam@gmail.com>
2023-03-08 20:09:27 +0800

    新增state globals

--- Makefile
+++ Makefile
@@ -2,7 +2,9 @@ CC := gcc
 CFLAGS_NP = -fcf-protection=none -fno-stack-protector
 
 make:
-	$(CC) sample/stack_over_flow/src/sof1.c -o sample/stack_over_flow/sof1_32bits $(CFLAPS_NP)
-	$(CC) sample/stack_over_flow/src/sof1.c -o sample/stack_over_flow/sof1_64bits $(CFLAGS_NP)
+	# mkdir sample/stack_over_flow/build
+
+	$(CC) sample/stack_over_flow/src/sof1.c -o sample/stack_over_flow/build/sof1_32bits $(CFLAPS_NP)
+	$(CC) sample/stack_over_flow/src/sof1.c -o sample/stack_over_flow/build/sof1_64bits $(CFLAGS_NP)
 clean:
 	rm sample/stack_over_flow/sof1_32bits sample/stack_over_flow/sof1_64bits
--- lib/StackOverFlow.py
+++ lib/StackOverFlow.py
@@ -2,37 +2,70 @@ import angr
 
 def check_head(act):
     block = act.project.factory.block(act.addr)
-    # print(block.pp())
+    print(block.pp())
     insns = block.capstone.insns
     
     # find push rbp; mov rbp, rsp
-    if(len(insns)>=2):
-        insns1=insns[0]
-        insns2=insns[1]
-        if( not (insns1.mnemonic=="push" and insns2.mnemonic=="mov") ):
-            return
-        op_str1 = insns1.op_str.split(", ")
-        op_str2 = insns2.op_str.split(", ")
-        if( not (len(op_str1)==1 and len(op_str2)==2) ):
-            return
-        if( not (op_str1[0]=="rbp" and op_str2[0]=="rbp" and op_str2[1]=="rsp" )):
-            return
-        
-        print("Found head")
+    if( not (len(insns)>=2)):
+        return
+    
+    insns1=insns[0]
+    insns2=insns[1]
+    if( not (insns1.mnemonic=="push" and insns2.mnemonic=="mov") ):
+        return
+    op_str1 = insns1.op_str.split(", ")
+    op_str2 = insns2.op_str.split(", ")
+    if( not (len(op_str1)==1 and len(op_str2)==2) ):
+        return
+    if( not (op_str1[0]=="rbp" and op_str2[0]=="rbp" and op_str2[1]=="rsp" )):
+        return
+    
+    # 取得function 的return address
+    ret_addr = act.callstack.ret_addr
+    rbp=act.regs.rbp
+
+    # 儲存function 的 rbp, key 為 return address
+    act.globals['rbp_list'][ret_addr]=rbp
+    
+    print("Found head")
 
 def check_end(act):
     block = act.project.factory.block(act.addr)
     insns = block.capstone.insns
 
     # find leave; ret
-    if(len(insns)>=2):
-        insns1=insns[-2].mnemonic
-        insns2=insns[-1].mnemonic
-        # print(insns1, insns2)
-        if( not (insns1=="leave" and insns2=="ret") ):
-            return
-        print("Found end")
+    if( not (len(insns)>=2)):
+        return
+    
+    insns1=insns[-2].mnemonic
+    insns2=insns[-1].mnemonic
+    # print(insns1, insns2)
+    if( not (insns1=="leave" and insns2=="ret") ):
+        return
+    print("Found end")
+    
+    # check rbp symbolic
+    rbp=act.regs.rbp
+    rbp_stack = act.memory.load(rbp, 8, endness=angr.archinfo.Endness.LE)
+    ret_addr=act.callstack.ret_addr
+    origin_rbp=act.globals['rbp_list'][ret_addr]
 
+    print("origin", origin_rbp)
+    print("rbp", rbp)
+    
+    if(rbp_stack.symbolic):
+        act.memory.store(rbp, origin_rbp)
+        
+        print("rbp symbolic")
+
+    # ret_stack=act.memory.load(rbp+8, 8, endness=angr.archinfo.Endness.LE)
+    # if(ret_stack.symbolic):
+    #     print(act.memory.load(rbp+8, 8, endness=angr.archinfo.Endness.LE))
+    #     print(act.memory.load(ret_addr, 8, endness=angr.archinfo.Endness.LE))
+    #     # act.memory.store(rbp+8, ret_addr)
+
+    #     print("ret symbolic")
+        
 
 def StackOverFlow(file_path):
     proj = angr.Project(file_path, auto_load_libs=False)
@@ -42,6 +75,9 @@ def StackOverFlow(file_path):
             angr.options.SYMBOL_FILL_UNCONSTRAINED_REGISTERS
         }
     )
+    
+    # 儲存rbp
+    initial_state.globals['rbp_list']={}
 
     simgr = proj.factory.simgr(initial_state)
     while simgr.active:
